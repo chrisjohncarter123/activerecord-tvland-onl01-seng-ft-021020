7014d7284d4a4f44acd6011e876a94dd1427c046
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-21 23:47:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
-  has_one :day
-  has_one :genre
-  has_one :season
+
   
   def actors_list
     return self.actors.collect{|a| a.full_name}
