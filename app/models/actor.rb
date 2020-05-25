2c82530d48104e6b18febc24ad1511e6be8bbe67
github username <github email address>
2020-05-25 14:07:44 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        return (first_name  + " " + last_name)
+
+    end
+
+    def list_roles
+
+        result = []
+        characters.each do |c|
+            val = c.name + " - " + c.show.name
+            result << val
+
+        end
+
+        return result
+
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        result = []
+        actors.each do |c|
+            v = c.full_name
+            result << v
+
+
+        end
+        return result
+
+    end
   
 end
